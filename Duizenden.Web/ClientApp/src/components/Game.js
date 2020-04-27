a1c5f28234838fea34af9364824cf1277b0e843b
Robbert <robbert.ouwehand@gmail.com>
2020-04-27 20:53:40 +0200

    Submit form to add player to the game

--- Duizenden.Web/ClientApp/src/components/Game.js
+++ Duizenden.Web/ClientApp/src/components/Game.js
@@ -1,12 +1,34 @@
 ﻿import React, { Component } from "react";
+import PlayerApi from './../api/playerApi';
 
 export class Game extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            newPlayerName: ''
+        };
+    }
+
+    joinGame = (event) => {
+        PlayerApi.join(this.state.newPlayerName);
+        event.preventDefault();
+    };
+
+    updateInputValue = (evt) => {
+        this.setState({newPlayerName: evt.target.value});
+    };
+
     render() {
         return (
             <div>
                 <div>Da game</div>
-                <input type="text" /><br />
-                <button>Join!</button>
+                <form onSubmit={this.joinGame}>
+                    <label>
+                        Name: 
+                        <input type="text" value={this.state.newPlayerName} onChange={evt => this.updateInputValue(evt)} /><br />
+                    </label>
+                    <button onClick={this.joinGame}>Join!</button>
+                </form>
             </div>
         );
     }
