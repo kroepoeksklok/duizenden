9294e51e202336d708d0bb0c593f807fa4453017
Robbert <robbert.ouwehand@gmail.com>
2020-04-27 20:50:53 +0200

    Add API directory for apis with the backend

---
+++ Duizenden.Web/ClientApp/src/api/playerApi.js
@@ -0,0 +1,14 @@
+﻿import axios from 'axios';
+import * as constants from '../constants';
+
+class PlayerApi {
+    static join(playerName) {
+        console.log('join called', constants.WEBAPI_ROOT_URL);
+        const joinData = {
+            "playerName": playerName
+        };
+        axios.post(`${constants.WEBAPI_ROOT_URL}/player`, joinData);
+    }
+}
+
+export default PlayerApi;
